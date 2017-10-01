53852a5c8421c00ea03b11c2104ac8b84852cfa7
Elizabeta55 <kolmakova_17@mail.ru>
2017-10-01 20:47:06 +0300

    almost done homework4

---
+++ Homeworks/Task4/src/com/company/ArrayList.java
@@ -0,0 +1,108 @@
+package com.company;
+
+public class ArrayList {
+     final int InitialSize=100;
+     int elements[];
+     int count;
+
+//    void setElements(int elements,int count){
+//        this.elements[count] = elements;
+//    }
+
+//    void setCount(int count){
+//        if (count<100){
+//            this.count=count;
+//        } else {
+//            System.err.println("No free");
+//        }
+//    }
+
+    public ArrayList() {
+        elements = new int[InitialSize];
+        count = 0;
+    }
+
+    public void add(int element) {
+        if (count < elements.length) {
+            elements[count] = element;
+            count++;
+        } else {
+            System.err.println("No free");
+        }
+    }
+
+     public void addToBegin(int element){
+         for (int i = count - 1; i >= 0; i--) {
+             elements[i + 1] = elements[i];
+         }
+         elements[0] = element;
+         count++;
+         }
+
+//    int getCount(){
+//     return count;
+//     }
+
+//    int getElements(int index){
+//      return elements[index];
+//    }
+
+    public int get(int index) {
+       return elements[index];
+    }
+
+    public void replace(int index, int element) {
+         int temp;
+         for (int i=count;i>index;i--){
+             temp=elements[i+1];
+             elements[i]=elements[i-1];
+         }
+       elements[index]=element;
+        count++;
+     }
+
+    public void replace2(int index, int element) {
+        elements[index]=element;
+    }
+
+    public void sort(){
+        for (int i=0;i<count-1;i++){
+            for (int j=i+1;j<count;j++){
+                if (elements[j]>elements[i]){
+                    int temp=elements[j];
+                    elements[j]=elements[i];
+                    elements[i]=temp;
+                }
+            }
+        }
+    }
+
+    public void reverse(){
+        int temp;
+        for (int i=0;i<count/2;i++){
+            temp=elements[i];
+            elements[i]=elements[count-i-1];
+            elements[count-i-1]=temp;
+        }
+
+    }
+
+    public void remove(int index) {
+        for (int i = index; i < count; i++) {
+            elements[i] = elements[i + 1];
+        }
+        count--;
+    }
+
+    public int find(int element) {
+            int i=0;
+            while (elements[i]!=element && i<=count){
+                i++;
+            }
+            if (elements[i]==element){
+                return i;
+            } else {
+                return -1;
+            }
+    }
+}
---
+++ Homeworks/Task4/src/com/company/Main.java
@@ -0,0 +1,148 @@
+package com.company;
+
+import java.util.Scanner;
+import com.company.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList lists[] = new ArrayList[5];
+        boolean mark[] = new boolean[5];
+        for (int i=0;i<5;i++) {mark[i]=false;}
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Меню:");
+            System.out.println("1. Создать 1 из 5 списков");
+            System.out.println("2. Показать список.");
+            System.out.println("3. Работа с списком");
+            System.out.println("4. Закончить работу");
+            int command = scanner.nextInt();
+            switch (command){
+                case 1:
+                    System.out.println("Введите номер списка");
+                    int number=scanner.nextInt();
+                    mark[number]=true;
+                    if (number>5){
+                        System.err.println("Неверный вариант");
+                    } else {
+                        lists[number]=new ArrayList();
+                        while(true) {
+                            System.out.println("Добавить элемент в:");
+                            System.out.println("1. конец");
+                            System.out.println("2. начало");
+                            System.out.println("3. закончить");
+                            int command2=scanner.nextInt();
+                            switch (command2) {
+                                case 1:
+                                    System.out.println("Введите число, которое нужно добавить в конец списка");
+                                    int num = scanner.nextInt();
+                                    lists[number].add(num);
+                                    break;
+                                case 2:
+                                    System.out.println("Введите число, которое нужно добавить в начало списка");
+                                    num = scanner.nextInt();
+                                    lists[number].addToBegin(num);
+                                    break;
+                                case 3:
+                                    break;
+                            }
+                            if (command2==3) break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Введите номер списка, который хотите посмотреть");
+                    int num= scanner.nextInt();
+                    if (mark[num]!=true){
+                        System.err.println("Нет такого списка");
+                    } else {
+                        for(int i=0;i<lists[num].count;i++){
+                            System.out.println(lists[num].elements[i]);
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Введите номер списка");
+                    num= scanner.nextInt();
+                    if (mark[num]==true) {
+                        System.out.println("Выберите нужный вариант:");
+                        System.out.println("1. Вставить элемент в заданную позицию со сдвигом");
+                        System.out.println("2. Получить элемент по индексу");
+                        System.out.println("3. Заменить элемент в заданной позиции новым элементом");
+                        System.out.println("4. Отсортировать лист");
+                        System.out.println("5. Отобразить в обратном порядке");
+                        System.out.println("6. Удалить элемент");
+                        System.out.println("7. Найти индекс по элементу");
+                        System.out.println("8. Закончить работу");
+                        int command3 = scanner.nextInt();
+                        switch (command3) {
+                            case 1:
+                                System.out.println("Введите элемент:");
+                                int elem = scanner.nextInt();
+                                System.out.println("Введите позицию:");
+                                int index = scanner.nextInt();
+                                lists[num].replace(index,elem);
+                                System.out.println("Получили список:");
+                                for(int i=0;i<lists[num].count;i++) {
+                                    System.out.println(lists[num].elements[i]);
+                                }
+                                break;
+                            case 2:
+                                System.out.println("Введите индекс:");
+                                index = scanner.nextInt();
+                                System.out.println(lists[num].get(index));
+                                break;
+                            case 3:
+                                System.out.println("Введите элемент:");
+                                elem = scanner.nextInt();
+                                System.out.println("Введите позицию:");
+                                index = scanner.nextInt();
+                                lists[num].replace2(index,elem);
+                                System.out.println("Получили список:");
+                                for(int i=0;i<lists[num].count;i++) {
+                                    System.out.println(lists[num].elements[i]);
+                                }
+                                break;
+                            case 4:
+                                lists[num].sort();
+                                System.out.println("Получили список:");
+                                for(int i=0;i<lists[num].count;i++) {
+                                    System.out.println(lists[num].elements[i]);
+                                }
+                                break;
+                            case 5:
+                                lists[num].reverse();
+                                System.out.println("Получили список:");
+                                for(int i=0;i<lists[num].count;i++) {
+                                    System.out.println(lists[num].elements[i]);
+                                }
+                                break;
+                            case 6:
+                                System.out.println("Введите позицию удаляемого элемента:");
+                                index = scanner.nextInt();
+                                lists[num].remove(index);
+                                System.out.println("Получили список:");
+                                for(int i=0;i<lists[num].count;i++) {
+                                    System.out.println(lists[num].elements[i]);
+                                }
+                                break;
+                            case 7:
+                                System.out.println("Введите элемент:");
+                                elem = scanner.nextInt();
+                                System.out.println(lists[num].find(elem));
+                                break;
+                            case 8:
+                                break;
+                        }
+                        if (command3==8) break;
+                    } else{
+                        System.err.println("Нет такого списка");
+                    }
+                    break;
+                case 4:
+                    break;
+            }
+            if (command==4) break;
+        }
+    }
+}
